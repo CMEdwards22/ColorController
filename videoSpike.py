fa8a1f8aa995913688c4653ea19effab320eaf74
CMEdwards22 <edwardscm6@appstate.edu>
2022-01-24 00:34:31 -0500

    Created spike program to test out video

---
+++ videoSpike.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+import numpy as np
+
+# This code was inspired by and created from the example give from openCV documentation
+# Documentation example can be found here: https://docs.opencv.org/4.x/dd/d43/tutorial_py_video_display.html
+
+vidCap = cv.VideoCapture(0)  # Assigning video capture to variable, needs the 0 or it breaks (need to figure out why)
+if (not vidCap.isOpened()):  # statement to ensure camera is working
+    print("Error: Could not open camera, now exiting")
+    exit()
+
+while True:  # keep going until program is killed
+    # does a frame by frame capture into frame variable and if frame is read in corectly into frameTest boolean
+    frameTest, frame = vidCap.read()
+
+    if (not frameTest):  # Another statement to ensure that the frames are working
+        print("Error: Frame not read in correctly, now stopping program")
+        break
+
+    # Grayscales video
+    display = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    # Displays video in openCV GUI
+    cv.imshow('frame', display)
+    if cv.waitKey(1) == ord('q'):  # Not sure what this line does, I think it might just be a quit key
+        # turns out the wait key IS ABSOLUTELY NEEDED becuase you can't use the x button to quit.
+        break
+
+# Simple end of program stuff to prevent memory leaks
+vidCap.release()
+cv.destroyAllWindows()
