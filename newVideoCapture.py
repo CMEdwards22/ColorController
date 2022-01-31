966951af4fe26eb80083f48adafa12607aa04acc
CMEdwards22 <edwardscm6@appstate.edu>
2022-01-31 16:15:44 -0500

    Added getFrame function to newVideoCapture

--- newVideoCapture.py
+++ newVideoCapture.py
@@ -2,11 +2,20 @@ import cv2 as cv
 import numpy as np
 
 # newVideoCapture.py
-# Wrapper for cv.VideoCapture to include error checking
+# Includes wrappers for openCV video capture to include error checking
 
+# Wrapper for cv.VideoCapture to include error checking
 def getVideoCapture():
     vidCap = cv.VideoCapture(0)
     if (not vidCap.isOpened()):
         print("Error occured in getVideoCapture: Cannot open camera, now exiting")
         exit()
     return vidCap
+
+# Wrapper function for VideoCapture.read() to include error checking
+def getFrame(vidCap):
+    frameTest, frame = vidCap.read()
+    if (not frameTest):  # Another statement to ensure that the frames are working
+        print("Error: Frame not read in correctly, now stopping program")
+        exit()
+    return frame
