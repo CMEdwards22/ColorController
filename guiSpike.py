fff0e0180bff1499da45e7ccb4ddc35a366fc5c6
CMEdwards22 <edwardscm6@appstate.edu>
2022-02-21 15:50:13 -0500

    gui spike tested more, works now and is possible to directly connect the x y to the gui from the color tracker

--- guiSpike.py
+++ guiSpike.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import numpy as np
 
 import newVideoCapture as vc
 import colorMask as cm
@@ -9,7 +10,7 @@ import colorTracking as ct
 root = tk.Tk()
 root.title("GUI spike test")
 
-drawing = tk.Canvas(root, bg='white', height=300, width=300)
+drawing = tk.Canvas(root, bg='white', height=720, width=1280)
 
 #coord = 10, 10, 300, 300
 #arc = drawing.create_arc(coord, start=0, extent=150, fill="red")
@@ -17,9 +18,11 @@ drawing = tk.Canvas(root, bg='white', height=300, width=300)
 # Coords are as follows: x0,y0,x1,y1
 # x0 and y0 are the xy coords of the top left of circle/oval
 # x1 and y1 are the xy coords of the bottom right of circle/oval
-drawing.create_oval(60,60,210,210, fill="red")
+#drawing.create_oval(60,60,210,210, fill="red")
 
-
+# X Axis goes from left right with low values on left and high values on right
+# Y Axis goes from top down with low values on top and high values on bottom
+#drawing.create_oval(120, 60, 270, 210, fill="blue")
 
 
 # adds drawing canvas to window
@@ -32,5 +35,49 @@ drawing.pack()
 #print(frame.shape)
 
 # Same thing as root.mainloop()
+#while True:
+#    root.update()
+
+# root.destroy() can be used to kill gui like the destroywindows of openCV
+
+#vidCap = vc.getVideoCapture()
+
+# while True:
+#         hsvFrame = vc.hsvFrame(vidCap)
+#         frame = vc.getFrame(vidCap)
+#         mask = cm.getMask(hsvFrame, 255, 0, 0, itera=6)
+#         x,y,size,blobCount = ct.buildBlobTracker(frame, mask, 0, 30000) 
+#         cv.imshow("Frame with Color Tracking", frame)
+#         cv.imshow("Red Color Mask", mask)
+#         if blobCount > 0:
+#             #print("\n")
+#             #print("Total blob count: ", blobCount)
+#             #print("x : ", x)
+#             #print("y : ", y)
+#             #print("size : ", size)
+#             drawing.create_oval(x - (size / 2), y - (size / 2), x + (size / 2), y + (size / 2), fill="red", outline="")
+#         #else:
+#         #    print("No blobs detected")
+#         if cv.waitKey(1) == ord('q'):  
+#             break
+#         root.update()
+
+
+xtest = np.arange(1000)
+ytest = np.arange(1000)
+
+#print(type(xtest[1]))
+
+
+counter = 0
+y = 200
 while True:
+    #drawing.create_oval((xtest[counter] - 5, y - 5, xtest[counter] + 5, y + 5, outline="", fill="red")
+    drawing.create_oval(xtest[counter] - 15, 200, xtest[counter] - 15, 200, fill="red", outline="")
+    counter += 1
     root.update()
+    if cv.waitKey(1) == ord('q'):
+        break
+
+cv.destroyAllWindows()
+root.destroy()
